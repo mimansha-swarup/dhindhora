fdcc06b2cd81787300c8799c6f54980818fc13e4
mimansha-swarup <mimanshaswarup@gmail.com>
2022-05-30 18:15:36 +0530

    feat: user route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import { AllRoutes } from "./routes/Routes";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllUsers } from "./helper/user";
 function App() {
+  const {auth:{token}} = useSelector(state=>state)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if ( token) dispatch(getAllUsers());
+  }, [dispatch,token]);
   return (
     <div className="App">
       <AllRoutes />
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,62 +1,35 @@
-import {
-  Stack,
-  Avatar,
-  Typography,
-  Button,
-  Paper,
-  Divider,
-} from "@mui/material";
-import { blue, grey } from "@mui/material/colors";
+import { Typography } from "@mui/material";
+import { blue } from "@mui/material/colors";
 import { makeStyles } from "@mui/styles";
-import React from "react";
-import { HomeLayout, Post } from "../component";
+import { useParams } from "react-router-dom";
+import {  useSelector } from "react-redux";
+
+import { HomeLayout, Post, ProfileHeader } from "../component";
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "../utils";
+
+
 export const aLinkStyle = makeStyles({ color: blue[50] });
+
 export const ProfilePage = () => {
+  const { username } = useParams();
+  const {
+    user: { users },
+  } = useSelector((state) => state);
+
+  const [currUser, setCurrUser] = useState({ followers: [], following: [] });
+  
+  useEffect(() => {
+    
+    setCurrUser(getCurrentUser(users,username));
+    
+  }, [username, users]);
+
   return (
     <HomeLayout>
-      <Paper sx={{ p: 2 }}>
-        <Stack alignItems="center" gap={1}>
-          <Avatar
-            alt="profile-pic"
-            src="https://bestcellphonespyapps.com/wp-content/uploads/2017/09/pexels-photo-220453-1-1001x1024.jpeg"
-            sx={{ height: 156, width: 156 }}
-          />
-          <Typography fontWeight={500} variant="body1" component={"p"}>
-            Monkesh Kumar
-          </Typography>
-          <Typography variant="body2" component={"p"} color={grey[600]}>
-            @Monkeshxx
-          </Typography>
-          <Button variant="outlined" sx={{ borderRadius: 4 }} color="info">
-            Edit Profile
-          </Button>
-          <Typography variant="subtitle1" component={"p"} color={grey[700]}>
-            Monkesh the Monk | Not Influencer | Builder
-          </Typography>
-          <Typography variant="subtitle2" component={"p"} color={blue[300]}>
-            <a href="http://website" target="_blank" rel="noopener noreferrer">
-              {" "}
-              www.monkeshtemonk.com{" "}
-            </a>
-          </Typography>
-          <Stack b flexDirection="row" gap={2}>
-            <Stack>
-              Posts
-              <span>{0}</span>
-            </Stack>
-            <Divider orientation="vertical" flexItem />
-            <Stack>
-              Followers
-              <span>{0}</span>
-            </Stack>
-            <Divider orientation="vertical" flexItem />
-            <Stack>
-              Following
-              <span>{0}</span>
-            </Stack>
-          </Stack>
-        </Stack>
-      </Paper>
+     
+      <ProfileHeader currUser={currUser} />
+
       <Typography textAlign="start" my={2} variant="h6" component={"h6"}>
         Your Posts
       </Typography>
