8415fe7841556be1ce51c587b71618312e0a1715
mimansha-swarup <mimanshaswarup@gmail.com>
2022-05-30 18:16:00 +0530

    helpers

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 export const loginApi = "/api/auth/login";
 export const signUpApi = "/api/auth/signup";
 export const userApi = "/api/users";
+export const cloudinaryApi = "https://api.cloudinary.com/v1_1/daz8z0ksy/image/upload"
 
 
 export const concatedApi= (...args) => args.join("/");
