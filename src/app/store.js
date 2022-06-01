1db73a481afb08b14dfe2c982bd6e8229338d499
mimansha-swarup <mimanshaswarup@gmail.com>
2022-06-01 14:21:39 +0530

    feat: post Slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice"
-import userReducer from "../features/user/userSlice"
+import authReducer from "../features/auth/authSlice";
+import userReducer from "../features/user/userSlice";
+import postReducer from "../features/post/postSlice";
 
 export default configureStore({
-  reducer:{
+  reducer: {
     auth: authReducer,
     user: userReducer,
-  }
-})
+    post: postReducer,
+  },
+});
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 export const loginApi = "/api/auth/login";
 export const signUpApi = "/api/auth/signup";
 export const userApi = "/api/users";
+export const postApi = "/api/posts";
 export const cloudinaryApi = "https://api.cloudinary.com/v1_1/daz8z0ksy/image/upload"
 
 
